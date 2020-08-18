390897bcbca61d66367ba3980d2f60ad8f0cdccc
makutas <andrius.makutas@gmail.com>
2020-08-18 22:01:26 +0300

    reformat models file

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 
+class Recipe(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE(), null=True, blank=True)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.title}, posted by {self.author}'
+
+
 class Ingredient(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
@@ -11,13 +21,3 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return f'{self.name}, {self.ingredient_type}'
-
-
-class Recipe(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE(), null=True, blank=True)
-    title = models.CharField(max_length=100)
-    description = models.CharField(max_length=500)
-    created_date = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f'{self.title}, posted by {self.author}'
