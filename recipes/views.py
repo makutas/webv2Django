44a3e56a52cb5ebcc471fc30422350b965004c5e
makutas <andrius.makutas@gmail.com>
2020-08-20 23:09:41 +0300

    again

--- recipes/views.py
+++ recipes/views.py
@@ -10,7 +10,7 @@ def index(request):
 def recipes(request):
     """Show all recipes """
     recipes = Recipe.objects.order_by('created_date')
-    context = {'recipes':recipes}
+    context = {'recipes': recipes}
     return render(request, 'recipes/recipes.html', context)
 
 
@@ -18,5 +18,6 @@ def recipe(request, recipe_id):
     """Show a single recipe and all its ingredients"""
     recipe = Recipe.objects.get(id=recipe_id)
     ingredients = recipe.ingredient_set.order_by('ingredient_type')
-    context = {'recipe': recipe, 'ingredients': ingredients}
+    quantities = recipe.quantity_set.all()
+    context = {'recipe': recipe, 'ingredients': ingredients, 'quantity': quantities}
     return render(request, 'recipes/recipe.html', context)
